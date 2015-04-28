5a0b6048b8a3b9c50fc839a761b0e4225545fc31
Adrien Lo <adrien.lo@gmail.com>
2015-04-27 20:55:43 -0700

    break out json data

---
+++ json-data.js
@@ -0,0 +1,803 @@
+// just for prototype
+window.jsondata = [
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	},
+	{
+		'name': 'Scott Solomon',
+		'pos': 'EDGE',
+		'school': 'Rice',
+		'ht': 6030,
+		'wt': 262,
+		'vt': 31.63,
+		'foo': 9.50,
+		'fyd': 4.88
+	},
+	{
+		'name': 'Ben Gardner',
+		'pos': 'EDGE',
+		'school': 'Stanford',
+		'ht': 6041,
+		'wt': 262,
+		'vt': 30.75,
+		'foo': 9.50,
+		'fyd': 5.03
+	},
+	{
+		'name': 'Ricky Elmore',
+		'pos': 'EDGE',
+		'school': 'Arizona',
+		'ht': 6044,
+		'wt': 255,
+		'vt': 31.00,
+		'foo': 9.75,
+		'fyd': 4.84
+	},
+	{
+		'name': 'Matt Roth',
+		'pos': 'EDGE',
+		'school': 'Iowa',
+		'ht': 6036,
+		'wt': 278,
+		'vt': 30.88,
+		'foo': 10.00,
+		'fyd': 4.81
+	},
+	{
+		'name': 'Chris Ellis',
+		'pos': 'EDGE',
+		'school': 'Virgina Tech',
+		'ht': 6042,
+		'wt': 263,
+		'vt': 31.50,
+		'foo': 9.88,
+		'fyd': 4.71
+	}
+];
